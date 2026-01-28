4c466781fac19d0a91425ed5b7e9ba22b8ab70c8
Leonardo Yonamine <84002974+lenyon@users.noreply.github.com>
2026-01-27 23:52:06 -0300

    Adiciona useEffect e localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { initialState, taskReducer } from "./taskReducer";
 
 function App() {
@@ -12,6 +12,18 @@ function App() {
   function handleInputValue(e: React.ChangeEvent<HTMLInputElement>) {
     setTask(e.target.value);
   }
+
+  useEffect(() => {
+    const taskslist = localStorage.getItem("tasksList");
+
+    if (taskslist) {
+      dispatch({ type: "LOAD_TASKS", tasks: JSON.parse(taskslist) });
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasksList", JSON.stringify(state.tasks));
+  }, [state.tasks]);
 
   return (
     <div className="flex flex-col justify-center items-center min--w-screen min-h-screen gap-2">
@@ -32,7 +44,10 @@ function App() {
       <div className="rounded-md bg-zinc-600 p-2 min-w-md">
         {state.tasks.map((task) => (
           <ul key={task.id} className="text-neutral-50 list-disc">
-            <li>{task.text}</li>
+            <li>
+              {task.text}
+              {task.completed ? <span>✅</span> : <span>🚫</span>}
+            </li>
           </ul>
         ))}
       </div>
